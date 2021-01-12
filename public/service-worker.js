b3ec9ca273a39c408ed707803edb94c4ab865628
Elijam <elijam1608@gmail.com>
2021-01-12 14:31:12 +1100

    working on service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,22 @@
+  
+  const CACHE_NAME = "static-cache-bt-v1";
+  const DATA_CACHE_NAME = "data-cache-bt-v1";
+
+  const iconSize =['192' , '512'];
+  const iconFiles = iconSize.map(
+    (size) => '/icons/icon-${size}x${size}.png'
+  )
+
+
 const FILES_TO_CACHE = [
     "/",
-    "/index.html",
     "/index.js",
-    "/db.js",
-    "/styles.css",
+    '/favicon.ico',
     "/manifest.webmanifest",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512X512.png",
-  ];
+  ].concat(iconFiles);
   
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
-
+  
+// install
   self.addEventListener("install", function (evt) {
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
@@ -21,7 +26,7 @@ const FILES_TO_CACHE = [
     );
     self.skipWaiting();
   });
-  
+  // activate
   self.addEventListener("activate", function (evt) {
     evt.waitUntil(
       caches.keys().then(keyList => {
@@ -37,14 +42,16 @@ const FILES_TO_CACHE = [
     );
     self.clients.claim();
   });
+  // fetch
   self.addEventListener("fetch", function (evt) {
     if (evt.request.url.includes("/api/")) {
       evt.respondWith(
         caches.open(DATA_CACHE_NAME).then(cache => {
           return fetch(evt.request)
             .then(response => {
+              // the response was ok so clone and store it in cache
               if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
+                cache.put(evt.request, response.clone());
               }
               return response;
             })
